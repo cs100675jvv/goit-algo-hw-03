591968e99e120f45505d86cfc4a4a3609250af92
cs100675jvv <cs100675jvv@gmail.com>
2024-04-19 16:35:40 +0300

    task 3

---
+++ task3.py
@@ -0,0 +1,24 @@
+import re
+def normalize_phone(phone_number):
+    pattern = r"[^0-9]"
+    norm_number = re.split(pattern, phone_number)
+    norm_number = ''.join(norm_number)
+    if not norm_number.startswith('38'):
+        norm_number = "38" + norm_number
+    norm_number = "+" + norm_number
+    return norm_number
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
