41c57f298635b36de994e61a47cbe27d3f50e566
cs100675jvv <cs100675jvv@gmail.com>
2024-04-20 16:55:48 +0300

    added checking for wrong format

--- task1.py
+++ task1.py
@@ -5,7 +5,10 @@ def get_days_from_today(date):
     #current day
     current_date = datetime.now() 
     #given day in datetime
-    given_date = datetime.strptime(date, "%Y-%m-%d")  
+    try:
+        given_date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError ("vrong date format. Please enter date in format 'Year-month-day'")
     #difference between given day and today
     days_difference = given_date.toordinal()-current_date.toordinal()
     return days_difference
